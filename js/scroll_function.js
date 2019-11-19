2410c3ce0d07b587847b783249288a7bd5f2cea4
willmccarty13 <willmccarty13@gmail.com>
2019-11-18 23:48:11 -0500

    commit

--- js/scroll_function.js
+++ js/scroll_function.js
@@ -1,6 +1,6 @@
 $(function() {
     $('.scroll-down').click (function() {
-      $('html, body').animate({scrollTop: $('.container3').offset().top }, 'slow');
+      $('html, body').animate({scrollTop: $('.container1').offset().top }, 'slow');
       return false;
     });
   });
